45499dedd631b98a3cedd4c7128458893dc23f1d
Davide Ficano <davide.ficano@gmail.com>
2018-05-01 14:57:48 +0200

    fixed destination path

--- tensorflow/mk_syms.sh
+++ tensorflow/mk_syms.sh
@@ -4,14 +4,19 @@
 . ../config.sh
 
 ROOT_DIR=$IMAGE_DIR
-MATCHING_DIR=matching/c
+MATCHING_DIR=matching
+RESULTS_DIR=results
 
-mkdir -p $MATCHING_DIR
+function mk_syms {
+	mkdir -p $MATCHING_DIR/$1
 
-grep --null -Rl "$1" results | while IFS= read -r -d '' file; do
-    REL_PATH=`head -1 "$file" | sed 's/image: //'`
-    DIR_NAME=`dirname "$REL_PATH"`
-    FILE_NAME=`basename "$REL_PATH"`
-    # echo mkdir $MATCHING_DIR/$DIR_NAME
-    ln -s "$ROOT_DIR/$REL_PATH" $MATCHING_DIR/$FILE_NAME
-done
+	grep --null -Rl "$2" results/$1 | while IFS= read -r -d '' file; do
+	    REL_PATH=`head -1 "$file" | sed 's/image: //'`
+	    DIR_NAME=`dirname "$REL_PATH"`
+	    FILE_NAME=`basename "$REL_PATH"`
+	    ln -s "$ROOT_DIR/$REL_PATH" $MATCHING_DIR/$1/$FILE_NAME
+	done
+}
+
+mk_syms c "$1"
+mk_syms d "$1"
